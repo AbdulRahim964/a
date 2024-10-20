c440df66c8593c69e95f113b295b45da5bd7191b
AbdulRahim964 <rahimkhana343@gmail.com>
2024-10-20 15:05:39 +0000

    varible

---
+++ .devcontainer/basic/easy.js
@@ -0,0 +1,10 @@
+console.log("happy");
+console.log("joy");
+
+//             // Varible
+// const accountId = 12345;
+// let accountEmail = "abdul@gmail.com";
+// // var accountPassword = "54321";
+// // accountCity = "Asansol";
+
+// // accountId = 12345;
