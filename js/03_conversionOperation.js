ce1032422beae14988f74daa5a04bb99282bb658
AbdulRahim964 <rahimkhana343@gmail.com>
2024-10-21 17:41:46 +0000

    Operater

--- js/03_conversionOperation.js
+++ js/03_conversionOperation.js
@@ -28,23 +28,41 @@ let boolenIsLoggedIn = Boolean(isLoggedIn);
 // "Abdul Rahim Khan" => true;
 
 
+//************************************Operater****************************************
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+/* console.log(2 + 2);
+ console.log(2 - 2);
+ console.log(2 * 2);
+ console.log(2 / 2);
+ console.log(2 ** 2);
+ console.log(2 % 2);*/
+
+ let str1 = "Hello"
+let str2 = "Abdul"
+let str3 = str1 + str2
+
+// console.log(str3);
 
 
+/* console.log("1" + "2");
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2"); */
 
 
+/* console.log(true);
+console.log(+""); */
 
 
-
-
-
-
-
-
-
-
-
-
-
+let gameCounter = 100;
+//gameCounter++;
+++gameCounter;
+console.log(gameCounter);
 
 
 
