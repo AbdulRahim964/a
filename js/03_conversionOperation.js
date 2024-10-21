0b2ba7ad3922b0e4f78c47eb5d1f91cb29c897b9
AbdulRahim964 <rahimkhana343@gmail.com>
2024-10-21 15:53:29 +0000

    conversion of data types

---
+++ js/03_conversionOperation.js
@@ -0,0 +1,102 @@
+ let score = "A"
+
+/* console.log(typeof score);
+console.log(typeof(score)); */
+
+let valueInNunber = Number(score);
+/* console.log(typeof valueInNunber);
+console.log( valueInNunber); */
+
+                          //Note
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1;   false => 0;
+// null => 0;
+// undefined => NaN;
+// "Abdul Rahim Khan" => NaN;
+
+
+
+let isLoggedIn = " ";
+let boolenIsLoggedIn = Boolean(isLoggedIn);
+// console.log(boolenIsLoggedIn);
+
+               // Note
+// 1 => true;  0 => false;
+// " " => false;
+// "Abdul Rahim Khan" => true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
