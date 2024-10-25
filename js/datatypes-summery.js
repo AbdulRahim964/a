1e0d1ec4cdebbbea69cfcfb7e255914115a16ec0
AbdulRahim964 <rahimkhana343@gmail.com>
2024-10-25 14:10:16 +0000

    Stack and Heap

--- js/datatypes-summery.js
+++ js/datatypes-summery.js
@@ -1,4 +1,4 @@
-//  Premitive data type
+ //  Premitive data type
 //  7 types : String, Number, Boolearn, null, undefined, Sombol, BigInt
 
 /* const score = 100;
@@ -10,19 +10,19 @@ let userEmail;
 console.log(scoreValue); */
 
 
-const id = Symbol('123')
+/* const id = Symbol('123')
 const anotherId = Symbol('123')
 console.log(id === anotherId);
 
 const BigInt = 1234567890
-console.log(BigInt);
+console.log(BigInt); */
 
 
 
 //  Non Premitive data types
 //  Array, Object, Functions
 
-const heros = ["Shaktiman", "Naajraj", "Doga"]
+/* const heros = ["Shaktiman", "Naajraj", "Doga"]
  
 let myObj = {
     name: "Abdul Rahim Khan",
@@ -33,26 +33,34 @@ let myFunction= function () {
     console.log("hello world");
 }
 
-console.log(typeof 'myFunction');
+console.log(typeof 'myFunction'); */
 
 
 
 
+//   ********************************************************************************************
 
 
+//  Stack (Primitive),      Heap (Non Primitive)     
+
+let myName = "Abdul Rahim Khan";
+let myAnotherName = myName;
+ myAnotherName = "Raj";
+
+console.log(myName);
+console.log(myAnotherName);
 
 
+let userOne = {
+    email : "user@google.com",
+    upi : "user@PI"
+}
 
+let userTwo = userOne;
+userTwo.email = "abdulrahim@.com"
 
-
-
-
-
-
-
-
-
-
+console.log(userOne.email);
+console.log(userTwo.email);
 
 
 
