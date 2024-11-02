9b207f0f0f0b572f9175220b0b676df540816c7b
AbdulRahim964 <rahimkhana343@gmail.com>
2024-11-02 15:36:21 +0000

    Scope level and mini hoisting

--- 02_basics_js/Global_and_local_scope.js
+++ 02_basics_js/Global_and_local_scope.js
@@ -9,7 +9,56 @@ c = 30
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+
+
+                         // Scope level and mini hoisting
+
+
+/* function one() {
+    let username = "Abdul Rahim";
+
+    function two() {
+        let webside = "youtube";
+        console.log(username);
+    } 
+    // console.log(webside);
+    
+    // two()
+}
+
+one() */
+
+
+
+/* if (true) {
+    let username = "Raj"
+    if (username === "Raj") {
+        let webside = " instragram"
+    console.log(username + webside);
+    }
+    // console.log(webside);
+}
+
+// console.log(username); */
+
+
+
+// ************************************ Intersting *****************************
+
+
+
+console.log(addone(5));
+function addone(num){
+    return num + 1
+}
+
+addtwo(5)
+let addtwo = function(num){
+    return + 2
+}
+
 
 
 
