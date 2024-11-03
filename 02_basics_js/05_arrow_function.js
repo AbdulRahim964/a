0275e3cb3ff4bca904f49051942b2a59abc176a1
AbdulRahim964 <rahimkhana343@gmail.com>
2024-11-03 13:36:49 +0000

    using of this

---
+++ 02_basics_js/05_arrow_function.js
@@ -0,0 +1,24 @@
+let user = {
+    username: "Abdul Rahim",
+    price: 999,
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        
+    }
+}
+
+/* user.welcomeMessage()
+user.username = "Raj"
+user.welcomeMessage() */
+// console.log(welcomeMessage());
+
+
+
+
+
+
+
+
+
+
+
