8c4260a5ea895c33cacc6354538d0d20492867e2
AbdulRahim964 <rahimkhana343@gmail.com>
2024-12-02 20:35:37 +0530

    oop in JS

--- 02_basics_js/10_Classes_and_oop/oops.js
+++ 02_basics_js/10_Classes_and_oop/oops.js
@@ -1,16 +1,37 @@
-console.log("Starting");
-
 let user = {
-    
+   username: "Abdul Rahim Khan",
+   loginCount: 9, 
+   signedIn: true, 
+
+   getUserDetails: function(){
+    // console.log("Got user details from server");
+    // console.log(`Username: ${user.username}`);
+    // console.log(`Username: ${this.username}`);
+       console.log(this);
+            
+   }
 }
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
 
 
 
+function User (username, loginCount, isLoggedIn) {
+   this.username = username;
+   this.loginCount = loginCount;
+   this.isLoggedIn = isLoggedIn;
+
+   return this;
+}
+// console.log(User());
 
 
 
-
-
+let userOne = new User("Abdul Rahim Khan", 12, true )
+let userTwo = new User("Raj", 10, false )
+console.log(userOne);
+console.log(userTwo);
 
 
 
